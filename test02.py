94050efb5ecad3614052a6c5bd18153c6f0da946
Hallan-miranda <hallanlmm@gmail.com>
2024-08-30 16:41:49 -0300

    Removendo 12 da linha 11 e deixando 1

--- test02.py
+++ test02.py
@@ -8,7 +8,7 @@ sequencia = [0, n_inicial]
 
 while repetir < repeticao:
     #Adicona +1 a varialvel repetir, pega os ultimo e penultimo numero do array sequencia e adiciona a varialvel ultimo e penultimo
-    repetir += 12
+    repetir += 1
     ultimo = sequencia[-1]
     penultimo = sequencia[-2]
     # Soma os dois ultimos numeros do array sequencia e adiciona no final do array
