2743efe050ab2afb563998e7b9c0ada75ea7b036
Hallan-miranda <hallanlmm@gmail.com>
2024-09-10 16:14:29 -0300

    adicionando json forncedio pela empresa

--- test03.py
+++ test03.py
@@ -1,47 +1,161 @@
-faturamento_diario = [
-    2950, 0, 4600, 2700, 3500, 3300, 4100, 3800, 4200, 0,
-    3400, 2800, 4300, 0, 3700, 3000, 4600, 3900, 3100, 3200,
-    4000, 0, 4200, 3500, 2900, 3100, 3700, 0, 4400, 3400
+import json
+
+json_data = '''
+[
+	{
+		"dia": 1,
+		"valor": 22174.1664
+	},
+	{
+		"dia": 2,
+		"valor": 24537.6698
+	},
+	{
+		"dia": 3,
+		"valor": 26139.6134
+	},
+	{
+		"dia": 4,
+		"valor": 0.0
+	},
+	{
+		"dia": 5,
+		"valor": 0.0
+	},
+	{
+		"dia": 6,
+		"valor": 26742.6612
+	},
+	{
+		"dia": 7,
+		"valor": 0.0
+	},
+	{
+		"dia": 8,
+		"valor": 42889.2258
+	},
+	{
+		"dia": 9,
+		"valor": 46251.174
+	},
+	{
+		"dia": 10,
+		"valor": 11191.4722
+	},
+	{
+		"dia": 11,
+		"valor": 0.0
+	},
+	{
+		"dia": 12,
+		"valor": 0.0
+	},
+	{
+		"dia": 13,
+		"valor": 3847.4823
+	},
+	{
+		"dia": 14,
+		"valor": 373.7838
+	},
+	{
+		"dia": 15,
+		"valor": 2659.7563
+	},
+	{
+		"dia": 16,
+		"valor": 48924.2448
+	},
+	{
+		"dia": 17,
+		"valor": 18419.2614
+	},
+	{
+		"dia": 18,
+		"valor": 0.0
+	},
+	{
+		"dia": 19,
+		"valor": 0.0
+	},
+	{
+		"dia": 20,
+		"valor": 35240.1826
+	},
+	{
+		"dia": 21,
+		"valor": 43829.1667
+	},
+	{
+		"dia": 22,
+		"valor": 18235.6852
+	},
+	{
+		"dia": 23,
+		"valor": 4355.0662
+	},
+	{
+		"dia": 24,
+		"valor": 13327.1025
+	},
+	{
+		"dia": 25,
+		"valor": 0.0
+	},
+	{
+		"dia": 26,
+		"valor": 0.0
+	},
+	{
+		"dia": 27,
+		"valor": 25681.8318
+	},
+	{
+		"dia": 28,
+		"valor": 1718.1221
+	},
+	{
+		"dia": 29,
+		"valor": 13220.495
+	},
+	{
+		"dia": 30,
+		"valor": 8414.61
+	}
 ]
+'''
+
+faturamento_diario = json.loads(json_data)
 
 def maior_valor(valores):
     maior_valor = 0
-    repeat = len(valores)
-    index = 0
-    while repeat > index:
-        if faturamento_diario[index] > maior_valor:
-            maior_valor = faturamento_diario[index]
-        index += 1
+    for dia in valores:
+        if dia['valor'] > maior_valor:
+            maior_valor = dia['valor']
     print(f'Maior valor faturado em um dia foi de: R${maior_valor:.2f}')
 
 def menor_valor(valores):
     menor_valor = 100000
-    repeat = len(valores)
-    index = 0
-    while repeat > index:
-        if faturamento_diario[index] < menor_valor and faturamento_diario[index] > 0:
-            menor_valor = faturamento_diario[index]
-        index += 1
+    for dia in valores:
+        if dia['valor'] < menor_valor and dia['valor'] != 0:
+            menor_valor = dia['valor']
     print(f'Menor valor faturado em um dia foi de: R${menor_valor:.2f}')
 
 def superio_media(valores):
     total_faturado= 0
     superior_media = 0
     dias_faturados = 0
-    repeat = len(valores)
-    index = 0
-    
-    while repeat > index:
-        if valores[index] > 0:
-            total_faturado += valores[index]
+    for dia in valores:
+        if dia['valor'] > 0:
+            total_faturado += dia['valor']
             dias_faturados += 1
-        index += 1
+    
     media_diaria = total_faturado / dias_faturados
-    index = 0
-    while repeat > index:
-        if valores[index] > media_diaria:
+
+    for dia in valores:
+        if dia['valor'] > media_diaria:
             superior_media += 1
-        index += 1
+
     print(f'Total de dias de faturamento superior a média diária: {superior_media} dias')
     print(f'Media diaria faturada: R${media_diaria:.2f}')
 
